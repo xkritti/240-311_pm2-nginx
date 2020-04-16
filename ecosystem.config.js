7720f4976f866f0d9362d0a0e9a278e515be29c9
ZenpoDev <krittamet0404@gmail.com>
2020-04-16 16:45:08 +0700

    add docker.conf,docker-compose.yml for use NGINX & add json petshop data

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apps : [{
-    name: 'LAB9',
+  apps: [{
+    name: 'Petshop',
     script: 'server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
@@ -11,22 +11,12 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
-      PORT:3000
+      PORT: 3000
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT:80
+      PORT: 80
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 let express = require('express');
 let app = express();
 
+let petshop = [
+    {
+        id: 0,
+        brand: "Purina One",
+        name: "Purina One Indoor Advantage",
+        type: "FOOD",
+        price: 319
+
+    },
+    {
+        id: 1,
+        brand: "CATTOY",
+        name: "Kanimal Cat Teaser",
+        type: "TOY",
+        price: 89
+
+    },
+    {
+        id: 2,
+        brand: "Dental Fresh",
+        name: "Dental Fresh Original Formula for Cats",
+        type: "HEALTHY",
+        price: 280
+
+    }
+]
+
 app.get('/', (req, res) => {
-    console.log('Data Send!!')
-    res.send('Client Lab9')
+    console.log('Home Page !!')
+    res.send("Hello Home Page")
+});
+app.get('/petshop', (req, res) => {
+    console.log('Petshop Page !!')
+    res.json(petshop)
 });
 
 app.get('/crash', (req, res) => {
@@ -14,4 +45,5 @@ app.get('/crash', (req, res) => {
 
 })
 
-app.listen(80, () => console.log('Server running at PORT', 80));
+app.listen(process.env.PORT, 
+    () => console.log('Server running at PORT', process.env.PORT));
